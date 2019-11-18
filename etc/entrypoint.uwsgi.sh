3f78dae062d8feeeeb89a4ae18f95964fd2430e1
suecharo <suehiro619@gmail.com>
2019-11-18 23:29:49 +0000

    Modify entrypoint

--- etc/entrypoint.uwsgi.sh
+++ etc/entrypoint.uwsgi.sh
@@ -4,5 +4,4 @@ wait-for sapporo-web-database:5432
 python3 /opt/SAPPORO/SAPPORO-web/src/manage.py makemigrations app
 python3 /opt/SAPPORO/SAPPORO-web/src/manage.py migrate
 python3 /opt/SAPPORO/SAPPORO-web/src/manage.py collectstatic --noinput
-touch /opt/SAPPORO/SAPPORO-web/src/static/.gitkeep
 uwsgi /opt/SAPPORO/SAPPORO-web/etc/uwsgi/uwsgi.ini
