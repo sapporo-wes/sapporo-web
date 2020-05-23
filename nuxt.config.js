2f8435561da223a8120ce27ca7c8c1a3a62dbee1
suecharo <suehiro619@gmail.com>
2020-05-24 00:58:56 +0900

    Nuxt create app

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'spa',
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+  modules: ['@nuxtjs/axios'],
+  axios: {},
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+  build: {
+    extend(config, ctx) {}
+  },
+  srcDir: './src/'
+}
