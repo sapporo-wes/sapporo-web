916013a48241a69aeb30d0fbcc9827acc570312f
suecharo <suehiro619@gmail.com>
2020-07-30 12:50:08 +0900

    Update

--- src/store/service.ts
+++ src/store/service.ts
@@ -1,6 +1,7 @@
 import { ActionContext, ActionTree, GetterTree, MutationTree } from 'vuex'
 import { RootState } from '@/store'
-import { Service, WorkflowEngine } from '@/types'
+import { Service } from '@/types'
+import { getServiceInfo } from '@/util/WESRequest'
 import { v4 as uuidv4 } from 'uuid'
 
 type State = {
@@ -15,29 +16,39 @@ export const getters: GetterTree<State, RootState> = {
   serviceNames(state: State): string[] {
     return state.services.map((service: Service) => service.name)
   },
+
   existName: (state: State) => (serviceName: string): boolean => {
     return state.services
       .map((service: Service) => service.name)
       .includes(serviceName)
   },
+
   serviceIds(state: State): string[] {
-    return state.services.map((service: Service) => service.uuid)
+    return state.services.map((service: Service) => service.id)
   },
-  serviceFilterId: (state: State) => (serviceId: string): Service => {
+
+  serviceFilteredById: (state: State) => (serviceId: string): Service => {
     return state.services.filter(
-      (service: Service) => service.uuid === serviceId
+      (service: Service) => service.id === serviceId
     )[0]
   },
-  workflowEngines: (state: State) => (serviceId: string): WorkflowEngine[] => {
+
+  workflowEngines: (state: State) => (
+    serviceId: string
+  ): { name: string; version: string }[] => {
     const service = state.services.filter(
-      (service: Service) => service.uuid === serviceId
+      (service: Service) => service.id === serviceId
     )[0]
-    return Object.entries(service.workflowEngineVersions).map(
-      ([engineName, engineVersion]: [string, string]): WorkflowEngine => ({
-        name: engineName,
-        version: engineVersion
-      })
-    )
+    if (typeof service !== 'undefined') {
+      return Object.entries(service.serviceInfo.workflow_engine_versions).map(
+        ([name, version]: [string, string]) => ({
+          name,
+          version
+        })
+      )
+    }
+
+    return []
   }
 }
 
@@ -45,71 +56,58 @@ export const mutations: MutationTree<State> = {
   clearServices(state: State): void {
     state.services = []
   },
+
   setServices(state: State, services: Service[]): void {
     state.services = services
   },
+
   addService(state: State, service: Service): void {
     state.services.push(service)
   }
-}
-
-type SubmittedService = {
-  name: string
-  endpoint: string
 }
 
 export const actions: ActionTree<State, RootState> = {
   async clearServices({ commit }: ActionContext<State, any>) {
     commit('clearServices')
   },
+
   async submitService(
     { commit }: ActionContext<State, any>,
-    service: SubmittedService
+    service: { name: string; endpoint: string }
   ): Promise<string> {
-    const res = await this.$axios
-      .$get(`${service.endpoint}/service-info`)
-      .catch(() => {
-        throw new Error('An error occurred on the entered service endpoint.')
-      })
+    const serviceInfo = await getServiceInfo(
+      this.$axios,
+      service.endpoint
+    ).catch((e) => {
+      throw e
+    })
+
     const serviceId: string = uuidv4()
     commit('addService', {
       name: service.name,
       endpoint: service.endpoint,
       state: 'Available',
       addedDate: new Date(),
-      uuid: serviceId,
+      id: serviceId,
       workflowIds: [],
-      authInstructionsUrl: res.auth_instructions_url || '',
-      contactInfoUrl: res.contact_info_url || '',
-      defaultWorkflowEngineParameters:
-        res.default_workflow_engine_parameters || [],
-      supportedFilesystemProtocols: res.supported_filesystem_protocols || [],
-      supportedWesVersions: res.supported_wes_versions || [],
-      systemStateCounts: res.system_state_counts || {},
-      tags: res.tags || {},
-      workflowEngineVersions: res.workflow_engine_versions || [],
-      workflowTypeVersions: Object.entries(res.workflow_type_versions).reduce(
-        (acc, [key, item]: [string, any]) => ({
-          ...acc,
-          [key]: { workflowTypeVersion: item.workflow_type_version }
-        }),
-        {}
-      )
+      serviceInfo
     })
+
     return serviceId
   },
+
   async deleteServices(
     { commit, state, dispatch }: ActionContext<State, any>,
     serviceIds: string[]
   ): Promise<void> {
     commit(
       'setServices',
-      state.services.filter((service) => !serviceIds.includes(service.uuid))
+      state.services.filter((service) => !serviceIds.includes(service.id))
     )
     await dispatch(
       'workflow/deleteWorkflows',
       state.services
-        .filter((service) => serviceIds.includes(service.uuid))
+        .filter((service) => serviceIds.includes(service.id))
         .map((service) => service.workflowIds)
         .flat(),
       { root: true }
--- src/store/workflow.ts
+++ src/store/workflow.ts
@@ -1,9 +1,9 @@
 import { ActionContext, ActionTree, GetterTree, MutationTree } from 'vuex'
-import { extractWorkflowParameters } from '@/util/extractWorkflowParameters'
-import { inspectWorkflow } from '@/util/inspectWorkflow'
+import { readFile } from '@/util'
+import { extractWorkflowParameters, inspectWorkflow } from '@/util/agodashi'
 import { RootState } from '@/store'
-import { v4 as uuidv4 } from 'uuid'
 import { Workflow } from '@/types'
+import { v4 as uuidv4 } from 'uuid'
 
 type State = {
   workflows: Workflow[]
@@ -17,19 +17,22 @@ export const getters: GetterTree<State, RootState> = {
   workflowNames(state: State): string[] {
     return state.workflows.map((workflow) => workflow.name)
   },
+
   existName: (state: State) => (workflowName: string): boolean => {
     return state.workflows
       .map((workflow: Workflow) => workflow.name)
       .includes(workflowName)
   },
+
   existWorkflowId: (state: State) => (workflowId: string) => {
     return state.workflows
-      .map((workflow: Workflow) => workflow.uuid)
+      .map((workflow: Workflow) => workflow.id)
       .includes(workflowId)
   },
-  workflowFilterId: (state: State) => (workflowId: string): Workflow => {
+
+  workflowFilteredById: (state: State) => (workflowId: string): Workflow => {
     return state.workflows.filter(
-      (workflow: Workflow) => workflow.uuid === workflowId
+      (workflow: Workflow) => workflow.id === workflowId
     )[0]
   }
 }
@@ -38,88 +41,94 @@ export const mutations: MutationTree<State> = {
   clearWorkflows(state: State): void {
     state.workflows = []
   },
+
   setWorkflows(state: State, workflows: Workflow[]): void {
     state.workflows = workflows
   },
+
   addWorkflow(state: State, workflow: Workflow): void {
     state.workflows.push(workflow)
-  }
-}
+  },
 
-type SubmittedWorkflow = {
-  serviceId: string
-  name: string
-  url: string
-  file: File | undefined
+  addRunId(state: State, payload: { workflowId: string; runId: string }) {
+    for (let i = 0; i++; i < state.workflows.length) {
+      const workflow = state.workflows[i]
+      if (workflow.id === payload.workflowId) {
+        state.workflows[i].runIds.push(payload.runId)
+        break
+      }
+    }
+  }
 }
 
 export const actions: ActionTree<State, RootState> = {
   async clearWorkflows({ commit }: ActionContext<State, any>) {
     commit('clearWorkflows')
   },
+
   async submitWorkflow(
     { commit }: ActionContext<State, any>,
-    workflow: SubmittedWorkflow
+    workflow: {
+      serviceId: string
+      name: string
+      url?: string
+      file?: File
+    }
   ) {
-    let content: string
-    if (typeof workflow.file === 'undefined') {
+    let content
+    if (typeof workflow.url !== 'undefined') {
       content = await this.$axios.$get(workflow.url).catch(() => {
         throw new Error('An error has occurred on the entered workflow url.')
       })
-    } else {
+    }
+    if (typeof workflow.file !== 'undefined') {
       content = await readFile(workflow.file).catch((e) => {
         throw e
       })
     }
-    const workflowTypeVersion = inspectWorkflow(content)
-    const workflowParameters = extractWorkflowParameters(
-      content,
-      workflowTypeVersion.type
-    )
+
+    const wfTypeVersion = inspectWorkflow(content)
+    const wfParams = extractWorkflowParameters(content)
     const workflowId: string = uuidv4()
     commit('addWorkflow', {
       name: workflow.name,
-      type: workflowTypeVersion.type,
-      version: workflowTypeVersion.version,
-      url: workflow.url,
-      fileName: workflow.file?.name || '',
+      type: wfTypeVersion.type,
+      version: wfTypeVersion.version,
+      url: workflow?.url,
+      fileName: workflow?.file?.name,
       content: content,
-      params: workflowParameters,
+      params: wfParams,
       addedDate: new Date(),
       serviceId: workflow.serviceId,
       uuid: workflowId,
       runIds: []
     })
+
     return workflowId
   },
+
   async deleteWorkflows(
     { commit, state, dispatch }: ActionContext<State, any>,
     workflowIds: string[]
   ): Promise<void> {
     commit(
       'setWorkflows',
-      state.workflows.filter((workflow) => !workflowIds.includes(workflow.uuid))
+      state.workflows.filter((workflow) => !workflowIds.includes(workflow.id))
     )
     await dispatch(
       'run/deleteRuns',
       state.workflows
-        .filter((workflow) => workflowIds.includes(workflow.uuid))
+        .filter((workflow) => workflowIds.includes(workflow.id))
         .map((workflow) => workflow.runIds)
         .flat(),
       { root: true }
     )
-  }
-}
+  },
 
-const readFile = async (file: File): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    let reader = new FileReader()
-    reader.onload = () => {
-      resolve(reader.result as string)
-    }
-    reader.onerror = () => {
-      reject(reader.error)
-    }
-    reader.readAsText(file)
-  })
+  async addRunId(
+    { commit }: ActionContext<State, any>,
+    payload: { workflowId: string; runId: string }
+  ): Promise<void> {
+    commit('addRunId', payload)
+  }
 }
