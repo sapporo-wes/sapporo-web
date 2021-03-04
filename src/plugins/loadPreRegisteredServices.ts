f545ac4a01ce305b4c897e7d2ac28197dab8373a
suecharo <suehiro619@gmail.com>
2021-03-04 16:21:02 +0900

    Add the func to load the pre-registered service

---
+++ src/plugins/loadPreRegisteredServices.ts
@@ -0,0 +1,26 @@
+import preRegisteredServices from '@/assets/preRegisteredServices.json'
+import { Service } from '@/store/services'
+import { Context } from '@nuxt/types'
+
+export interface MyWindow extends Window {
+  onNuxtReady(obj: () => void): void
+}
+declare let window: MyWindow
+
+export default (context: Context): void => {
+  window.onNuxtReady(async () => {
+    if (context.env?.LOAD_PRE_REGISTERED_SERVICES === "'true'") {
+      const services: Service[] = context.store.getters['services/services']
+      const serviceNames = services.map((service) => service.name)
+      for (const preRegisteredService of preRegisteredServices) {
+        if (!serviceNames.includes(preRegisteredService.name)) {
+          await context.store.dispatch('services/submitService', {
+            name: preRegisteredService.name,
+            endpoint: preRegisteredService.endpoint,
+            preRegistered: true,
+          })
+        }
+      }
+    }
+  })
+}
