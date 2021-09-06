259eab31c7dd148ba409ef7e6ed11412158e1fa3
Tazro Inutano Ohta <inutano@gmail.com>
2021-09-06 14:41:25 +0900

    not delete, deregister

--- src/components/index/ServiceDeleteDialog.vue
+++ src/components/index/ServiceDeleteDialog.vue
@@ -6,29 +6,26 @@
     @click:outside="$emit('close')"
   >
     <v-card>
-      <div class="card-header pl-6 pt-4" v-text="'Delete Services'" />
-      <div class="px-12 py-2" v-text="'These services will be deleted.'" />
+      <div class="card-header pl-6 pt-4" v-text="'Deregister WES Services'" />
+      <div class="px-12 py-2" v-text="'Following WES services will be removed:'" />
       <ul :style="{ paddingLeft: '96px', paddingRight: '96px' }">
         <li v-for="(item, i) in selectedItems" :key="i" v-text="item.name" />
       </ul>
-      <div
-        class="px-12 py-2"
-        v-text="
-          'Workflows and runs associated with these services will also be deleted.'
-        "
-      />
-      <div
+      <div class="px-12 py-2">
+        Workflows and runs associated with these services will be removed. You can re-register the services, but changes on workflows and runs will NOT be recovered.
+      </div>
+      <!-- <div
         :style="{
           fontSize: '1.4rem',
           color: $vuetify.theme.themes.light.error,
         }"
         class="text-center"
         v-text="'Are you sure to delete these?'"
-      />
+      /> -->
       <div class="d-flex justify-end px-12 pt-4 pb-6">
         <v-btn color="error" outlined @click.stop="deleteSelectedItems">
           <v-icon class="mr-2" v-text="'mdi-trash-can-outline'" />
-          <span v-text="'Delete'" />
+          <span v-text="'Deregister'" />
         </v-btn>
         <v-btn color="info" class="ml-4" outlined @click.stop="$emit('close')">
           <v-icon class="mr-2" v-text="'mdi-close'" />
